8185a128f74bf61e9629c5d26790c4d5675d5269
707BIGBOSS\user <2001mist@naver.com>
2022-05-01 23:29:39 +0900

    mariaDB연습시작

---
+++ mariaDB/연습용 데이타.sql
@@ -0,0 +1,19 @@
+TRUNCATE TABLE your_table; -- 테이블 삭제
+
+INSERT INTO your_table (uid, name, age, birthday, isnosql) VALUES
+
+    ('1908001', '오라클', 43, '1977-01-01', 0);
+
+INSERT INTO your_table (uid, name, age, birthday, isnosql) VALUES
+
+    ('1908002', 'PostgreSQL', 31, '1989-01-01', 0),
+
+    ('1911001', 'MySQL', 25, '1995-05-23', 0),
+
+    ('1911002', 'MongoDB', 13, '2009-01-01', 1),
+
+    ('1911003', '마리아DB', 11, '2009-01-22', 0),
+
+    ('2001001', '아파치 카산드라', 12, '2008-01-01', 1),
+
+    ('2001002', '소프트웨어 알림장', NULL, '2017-11-01', 0);
---
+++ mariaDB/테이블삭제.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE your_table; -- 테이블 삭제
+
+INSERT INTO your_table(uid, NAME, age, birthday, isnosql) VALUES ('1908001', '마리아db', '25', '2002-01-01', 0);
+
+
